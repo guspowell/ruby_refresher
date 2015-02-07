f54d9c0ac2e1931cb75ef75d0c497fa543708c47
guspowell <augustuspowell@me.com>
2015-02-07 13:45:18 +0000

    third test passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -25,6 +25,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.delete_if { |element| element == nil || element == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
