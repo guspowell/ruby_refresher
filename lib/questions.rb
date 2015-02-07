c43f8d8c321e08265c0eebc060f9da5eebc77e3e
guspowell <augustuspowell@me.com>
2015-02-07 15:45:56 +0000

    array repeated twice in array passed

--- lib/questions.rb
+++ lib/questions.rb
@@ -117,6 +117,8 @@ end
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array = [[array],[array]]
+  array.flatten!
 end
 
 # convert a symbol into a string
